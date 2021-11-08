94b26735116a2d612b7e5321788e925e9c76f507
Devin Ragotzy <devin.ragotzy@gmail.com>
2021-11-08 03:14:04 -0500

    Rerun a few files to test bin for turn in

--- readme.md
+++ readme.md
@@ -1,17 +1,16 @@
 # Hello Dr. Carr ;)
 
 There is no need to run `./compile`; the PEG parser uses a fancy Rust macro to generate it when I compile it.
-So just `./run input/foo.cm` will work and `./run input/foo.cm input/bar.cm input/baz.cm` will also work.
+So just `./run input/foo.cm` will work and `./run input/foo.cm input/bar.cm input/baz.cm` will also work. The assembly files will be in
+`./input/build/foo.s`
 
-The `grammar.pest` file is the `CminusSkeleton.g4` equivalent (PEG grammar is slightly different). The
-precedence of expressions is defined partially in the grammar file but more so using precedence climbing
-[here](./src/ast/parse.rs) in the function `parse_expr` and the precedence climbing algorithm [here](./src/precedence.rs).
-
-I tested that I didn't disturb my existing output (your test files) by doing a sort of diff of my compiler error messages. The rust compiler has +10,000 of these kinds of tests it is really helpful to make sure you don't lose or add failure modes to the compiler. (see the added `./input.stderr`)file
+I tested that I didn't disturb my existing output (your test files) by doing a sort of diff of my compiler error messages. The rust compiler has +10,000 of these kinds of tests. They are really helpful to make sure you don't lose or add failure modes to the compiler. (see the added `./input.stderr`)file
 
 ## I have added
 
-- a `./input/stuff` sub folder in input that has a bunch of passing and failing test files
+- a `./stuff` sub folder in input that has a bunch of passing and failing test files
+`asmgen` is test for codegen and `parse_ui` is parsing/type checking/polymorphization/monomorphization tests
+the `./stuff/idea.cm` is just ideas for where this will go.
 
 
 ### I added
@@ -23,3 +22,5 @@ and implementing would look like
 `impl add<struct point> { add(struct point this, struct point other) { this.x += other.x } }`
 
 I would still like to remove coercion and replace it with conversion traits like the [into trait](./input/stuff/trait/into.cm)
+
+I am going to remove variable declaration then instantiation next time so, all my input files will be adjusted but not this time.
--- stuff/asmgen/add/build/sub.s
+++ stuff/asmgen/add/build/sub.s
@@ -16,50 +16,45 @@
 .type main,@function
 
 main:
-    pushq %rbp
-    mov %rsp, %rbp
-    pushq $1
-    pushq $1
-    pushq $1
-    pushq $1
-    pushq $1
-    movq $3, -32(%rbp)
-    movq $10, -8(%rbp)
-    movq $5, -24(%rbp)
-    mov -24(%rbp), %rcx
-    imul -8(%rbp), %rcx
-    mov %rcx, %rdi
-    sub $3, %rdi
-    mov %rdi, %rcx
-    movq %rcx, -16(%rbp)
-    mov -16(%rbp), %rsi
-    mov $0, %rax
-    leaq .int_wformat(%rip), %rdi
-    call printf
-    mov -24(%rbp), %rcx
-    imul -8(%rbp), %rcx
-    mov %rcx, %rdi
-    sub -16(%rbp), %rdi
-    mov %rdi, %rcx
-    mov %rcx, %rsi
-    mov $0, %rax
-    leaq .int_wformat(%rip), %rdi
-    call printf
-    movq $3, %rdi
-    sub -24(%rbp), %rdi
-    mov %rdi, %r12
-    mov %r12, %rsi
-    mov $0, %rax
-    leaq .int_wformat(%rip), %rdi
-    call printf
-    mov -24(%rbp), %rcx
-    add $4, %rcx
-    mov %rcx, %rdi
-    movq %rdi, -40(%rbp)
-    mov -40(%rbp), %rsi
-    mov $0, %rax
-    leaq .int_wformat(%rip), %rdi
-    call printf
-    movq $0, %rax
+    pushq                    %rbp                             # 
+    mov                      %rsp,                %rbp        # 
+    pushq                      $1                             # 
+    pushq                      $1                             # 
+    pushq                      $1                             # 
+    pushq                      $1                             # 
+    pushq                      $1                             # 
+    movq                       $3,           -32(%rbp)
+    movq                      $10,            -8(%rbp)
+    movq                       $5,           -24(%rbp)
+    mov                 -24(%rbp),                %r13        # 
+    imul                 -8(%rbp),                %r13
+    sub                       $3,                %r13
+    movq                     %r13,           -16(%rbp)
+    mov                 -16(%rbp),                %rsi        # 
+    mov                        $0,                %rax        # 
+    leaq       .int_wformat(%rip),                %rdi
+    call                  printf
+    mov                 -24(%rbp),                %r13        # 
+    imul                 -8(%rbp),                %r13
+    sub                -16(%rbp),                %r13
+    mov                      %r13,                %rsi        # 
+    mov                        $0,                %rax        # 
+    leaq       .int_wformat(%rip),                %rdi
+    call                  printf
+    movq                       $3,                %rax
+    sub                -24(%rbp),                %rax
+    mov                      %rax,                %r13        # we had to spill a printf register
+    mov                      %r13,                %rsi        # 
+    mov                        $0,                %rax        # 
+    leaq       .int_wformat(%rip),                %rdi
+    call                  printf
+    mov                 -24(%rbp),                %r14        # 
+    add                       $4,                %r14
+    movq                     %r14,           -40(%rbp)
+    mov                 -40(%rbp),                %rsi        # 
+    mov                        $0,                %rax        # 
+    leaq       .int_wformat(%rip),                %rdi
+    call                  printf
+    movq                       $0,                %rax
     leave
     ret
