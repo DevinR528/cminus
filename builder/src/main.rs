6d8051799d1174d311e0cd7e53860befa40387ee
Devin Ragotzy <devin.ragotzy@gmail.com>
2021-11-14 22:27:34 -0500

    builder: update how the compiler works as a binary

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -14,13 +14,20 @@ fn main() {
         }
         ["run" | "r", more @ ..] => {
             cmd!("cargo b").run().unwrap();
-            if let Err(e) = build_files(more) {
+            if let Err(e) = build_files(more, "-as") {
+                eprintln!("{}", e);
+                std::process::exit(1);
+            }
+        }
+        ["debug" | "d", more @ ..] => {
+            cmd!("cargo b").run().unwrap();
+            if let Err(e) = build_files(more, "-asv") {
                 eprintln!("{}", e);
                 std::process::exit(1);
             }
         }
         ["asm" | "a", more @ ..] => {
-            if let Err(e) = build_files(more) {
+            if let Err(e) = build_files(more, "-a") {
                 eprintln!("{}", e);
                 std::process::exit(1);
             }
@@ -30,46 +37,55 @@ fn main() {
             let more: Vec<_> =
                 more.iter().filter(|n| n.parse::<usize>().is_err()).copied().collect();
             for _ in 0..count {
-                if let Err(e) = build_files(&more) {
+                if let Err(e) = build_files(&more, "-a") {
                     eprintln!("{}", e);
                     std::process::exit(1);
                 }
             }
         }
-        ["build" | "b", _more @ ..] => {
-            println!("{:?}", cmd!("cargo b").read());
+        ["build" | "b", more @ ..] => {
+            if let Err(e) = build_files(more, "") {
+                eprintln!("{}", e);
+                std::process::exit(1);
+            }
         }
         [..] => {}
     }
 }
 
-fn build_files(files: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
+fn build_files(files: &[&str], args: &str) -> Result<(), Box<dyn std::error::Error>> {
     for p in files {
         let path = PathBuf::from(p);
         if path.is_dir() {
             continue;
         }
-        cmd!("{ENUMC_DEBUG} {path}").run()?;
+        if args.is_empty() {
+            cmd!("{ENUMC_DEBUG} -i {path}").run()?;
+        } else {
+            cmd!("{ENUMC_DEBUG} {args} -i {path}").run()?;
+        }
 
-        let mut build_dir = path.clone();
-        let file = build_dir
-            .file_name()
-            .ok_or_else::<Box<dyn std::error::Error>, _>(|| "file not found".into())?
-            .to_os_string();
-        build_dir.pop();
-        build_dir.push("build");
-        build_dir.push(file);
+        if args.contains('a') {
+            let mut build_dir = path.clone();
+            let file = build_dir
+                .file_name()
+                .ok_or_else::<Box<dyn std::error::Error>, _>(|| "file not found".into())?
+                .to_os_string();
+            build_dir.pop();
+            build_dir.push("build");
+            build_dir.push(file);
 
-        let mut asm = build_dir.clone();
-        asm.set_extension("s");
+            let mut asm = build_dir.clone();
+            asm.set_extension("s");
 
-        let mut out = build_dir.clone();
-        out.set_extension("");
+            let mut out = build_dir.clone();
+            out.set_extension("");
 
-        cmd!("gcc -no-pie {asm} -o {out}").run()?;
-        if cmd!("{out}").read_stderr()?.contains("SEG") {
-            return Err("SIGSEGV assembly crashed with segmentation fault".into());
-        };
+            cmd!("gcc -no-pie {asm} -o {out}").run()?;
+            if cmd!("{out}").read_stderr()?.contains("SEG") {
+                return Err("SIGSEGV assembly crashed with segmentation fault".into());
+            };
+        }
     }
     Ok(())
 }
