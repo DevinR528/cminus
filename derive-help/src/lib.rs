7517ddaf8bb9a20e294758caaf64d1649ecc669e
Devin Ragotzy <devin.ragotzy@gmail.com>
2021-11-21 07:42:37 -0500

    derive-help: add format with attribute
    Allows formatting a field with a given function/method

--- derive-help/src/lib.rs
+++ derive-help/src/lib.rs
@@ -4,12 +4,12 @@ use proc_macro::TokenStream as StdTokenStream;
 use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
 use syn::{
-    parse_macro_input, parse_quote, spanned::Spanned, Attribute, Data, DeriveInput, Fields,
-    Generics, Type, Variant, WherePredicate,
+    parse_macro_input, parse_quote, spanned::Spanned, Attribute, Data, DeriveInput, Field, Fields,
+    Generics, Meta, NestedMeta, Type, Variant, WherePredicate,
 };
 
 /// Generates an implementation of `ruma_events::EventContent`.
-#[proc_macro_derive(Debug, attributes(dbg_ignore, with_fmt))]
+#[proc_macro_derive(Debug, attributes(dbg_ignore, dbg_with))]
 pub fn derive_event_content(input: StdTokenStream) -> StdTokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -18,17 +18,22 @@ pub fn derive_event_content(input: StdTokenStream) -> StdTokenStream {
     let fields = match input.data {
         Data::Struct(s) => fmt_field(&s.fields, name, &mut generics),
         Data::Enum(e) => {
-            let arms = e
+            let arms = match e
                 .variants
                 .iter()
-                .filter_map(|v| {
-                    if has_ignore(&v.attrs) {
-                        None
-                    } else {
-                        Some(variant_to_arm(v, &mut generics))
-                    }
+                .map(|v| {
+                    Ok(match has_attrs(&v.attrs)? {
+                        Attrs::Ignore => TokenStream::new(),
+                        Attrs::With(call) => variant_to_arm(v, Some(call), &mut generics),
+                        Attrs::None => variant_to_arm(v, None, &mut generics),
+                    })
                 })
-                .collect::<TokenStream>();
+                .collect::<syn::Result<TokenStream>>()
+                .map_err(|e| e.into_compile_error().into())
+            {
+                Ok(a) => a,
+                Err(tkns) => return tkns,
+            };
 
             quote! {
                 match self {
@@ -59,12 +64,32 @@ pub fn derive_event_content(input: StdTokenStream) -> StdTokenStream {
     .into()
 }
 
-fn variant_to_arm(variant: &Variant, gen: &mut Generics) -> TokenStream {
+fn variant_to_arm(
+    variant: &Variant,
+    with_call: Option<NestedMeta>,
+    gen: &mut Generics,
+) -> TokenStream {
+    let name = &variant.ident;
+
+    // So, `into_compile_error` just emits compile_error!($msg) so the surroundings have to be valid
+    // syntax...
+    let error = if with_call.is_some() && variant.fields.len() > 1 {
+        let err =
+            syn::Error::new(variant.span(), "can only call debug_with on one \"field\" at a time")
+                .into_compile_error();
+        return quote! {
+            Self::#name {.. } => {
+                #err
+            }
+        };
+    } else {
+        TokenStream::new()
+    };
+
     let gen_types = gen.type_params().into_iter().map(|t| t.ident.clone()).collect::<Vec<_>>();
     let mut done_gen = HashSet::new();
 
-    let name = &variant.ident;
-    let mut is_struct_like = false;
+    let mut has_named_fields = false;
     let (fields, debug_names): (TokenStream, TokenStream) = variant
         .fields
         .iter()
@@ -80,42 +105,132 @@ fn variant_to_arm(variant: &Variant, gen: &mut Generics) -> TokenStream {
                 }
             }
 
-            if has_ignore(&f.attrs) {
-                None
+            let attrs = match has_attrs(&f.attrs).map_err(|e| e.into_compile_error()) {
+                Ok(a) => a,
+                Err(e) => {
+                    return Some((e.clone(), e));
+                }
+            };
+
+            if with_call.is_some() {
+                Some(construct_debug_calls(f, idx, &mut has_named_fields, with_call.as_ref()))
             } else {
-                Some(if let Some(field_name) = &f.ident {
-                    is_struct_like = true;
-                    (
-                        quote! { #field_name, },
-                        quote! {
-                            let _ =
-                                ::core::fmt::DebugStruct::field(
-                                    dbg_builder,
-                                    stringify!(#field_name),
-                                    #field_name
-                                );
-                        },
-                    )
-                } else {
-                    let field_name = Ident::new(&format!("_self_{}", idx), f.span());
-                    let idx = syn::Index::from(idx);
-                    (
-                        quote! {
-                            #idx: #field_name,
-                        },
-                        quote! {
-                            let _ =
-                                ::core::fmt::DebugTuple::field(
-                                    dbg_builder,
-                                    #field_name
-                                );
-                        },
-                    )
-                })
+                match attrs {
+                    Attrs::Ignore => None,
+                    Attrs::With(expr) => {
+                        Some(construct_debug_calls(f, idx, &mut has_named_fields, Some(&expr)))
+                    }
+                    Attrs::None => Some(construct_debug_calls(f, idx, &mut has_named_fields, None)),
+                }
             }
         })
         .unzip();
-    let (start_fmtr, end_fmtr) = if is_struct_like {
+
+    let (start_fmtr, end_fmtr) = formatter_type(has_named_fields, name);
+    quote! {
+        Self::#name { #fields .. } => {
+            #error
+            #start_fmtr
+            #debug_names
+            #end_fmtr
+        }
+    }
+}
+
+fn fmt_field(fields: &Fields, name: &Ident, gen: &mut Generics) -> TokenStream {
+    let gen_types = gen.type_params().into_iter().map(|t| t.ident.clone()).collect::<Vec<_>>();
+    let mut done_gen = HashSet::new();
+
+    let mut has_named_fields = false;
+    let (fields, debug_names): (TokenStream, TokenStream) = fields
+        .iter()
+        .enumerate()
+        .filter_map(|(idx, f)| {
+            for path in peel_path(&f.ty) {
+                if !done_gen.insert(path.get_ident()) {
+                    continue;
+                }
+                if let Some(ty_name) = gen_types.iter().find(|id| Some(*id) == path.get_ident()) {
+                    let bound: WherePredicate = parse_quote! { #ty_name: ::core::fmt::Debug };
+                    gen.make_where_clause().predicates.extend([bound].into_iter())
+                }
+            }
+
+            let attrs = match has_attrs(&f.attrs).map_err(|e| e.into_compile_error()) {
+                Ok(a) => a,
+                Err(e) => {
+                    return Some((e.clone(), e));
+                }
+            };
+
+            match attrs {
+                Attrs::Ignore => None,
+                Attrs::With(expr) => {
+                    Some(construct_debug_calls(f, idx, &mut has_named_fields, Some(&expr)))
+                }
+                Attrs::None => Some(construct_debug_calls(f, idx, &mut has_named_fields, None)),
+            }
+        })
+        .unzip();
+
+    let (start_fmtr, end_fmtr) = formatter_type(has_named_fields, name);
+    quote! {
+        let #name { #fields .. } = &self;
+        #start_fmtr
+        #debug_names
+        #end_fmtr
+    }
+}
+
+fn construct_debug_calls(
+    f: &Field,
+    idx: usize,
+    is_struct_like: &mut bool,
+    with_call: Option<&NestedMeta>,
+) -> (TokenStream, TokenStream) {
+    if let Some(field_name) = &f.ident {
+        let field_or_call = if let Some(call) = with_call {
+            quote! { & #call (#field_name) }
+        } else {
+            quote! { #field_name }
+        };
+        *is_struct_like = true;
+        (
+            quote! { #field_name, },
+            quote! {
+                let _ =
+                    ::core::fmt::DebugStruct::field(
+                        dbg_builder,
+                        stringify!(#field_name),
+                        #field_or_call
+                    );
+            },
+        )
+    } else {
+        let field_name = Ident::new(&format!("_self_{}", idx), f.span());
+        let idx = syn::Index::from(idx);
+        let field_or_call = if let Some(call) = with_call {
+            quote! { & #call (#field_name) }
+        } else {
+            quote! { #field_name }
+        };
+        (
+            quote! {
+                #idx: #field_name,
+            },
+            quote! {
+                let _ =
+                    ::core::fmt::DebugTuple::field(
+                        dbg_builder,
+                        #field_or_call
+                    );
+            },
+        )
+    }
+}
+
+fn formatter_type(has_named_fields: bool, name: &Ident) -> (TokenStream, TokenStream) {
+    if has_named_fields {
         (
             quote! {
                 let dbg_builder = &mut ::core::fmt::Formatter::debug_struct(fmtr, stringify!(#name));
@@ -133,82 +248,6 @@ fn variant_to_arm(variant: &Variant, gen: &mut Generics) -> TokenStream {
                 ::core::fmt::DebugTuple::finish(dbg_builder)
             },
         )
-    };
-    quote! {
-        Self::#name { #fields .. } => {
-            #start_fmtr
-            #debug_names
-            #end_fmtr
-        }
-    }
-}
-
-fn fmt_field(fields: &Fields, name: &Ident, gen: &mut Generics) -> TokenStream {
-    let gen_types = gen.type_params().into_iter().map(|t| t.ident.clone()).collect::<Vec<_>>();
-    let mut done_gen = HashSet::new();
-
-    let mut is_struct_like = false;
-    let (fields, debug_names): (TokenStream, TokenStream) = fields
-        .iter()
-        .enumerate()
-        .filter_map(|(idx, f)| {
-            for path in peel_path(&f.ty) {
-                if !done_gen.insert(path.get_ident()) {
-                    continue;
-                }
-                if let Some(ty_name) = gen_types.iter().find(|id| Some(*id) == path.get_ident()) {
-                    let bound: WherePredicate = parse_quote! { #ty_name: ::core::fmt::Debug };
-                    gen.make_where_clause().predicates.extend([bound].into_iter())
-                }
-            }
-
-            if has_ignore(&f.attrs) {
-                None
-            } else {
-                Some(if let Some(field_name) = &f.ident {
-                    is_struct_like = true;
-                    (
-                        quote! { #field_name, },
-                        quote! {
-                            let _ =
-                                ::core::fmt::DebugStruct::field(
-                                    dbg_builder,
-                                    stringify!(#field_name),
-                                    #field_name
-                                );
-                        },
-                    )
-                } else {
-                    let field_name = Ident::new(&format!("_self_{}", idx), f.span());
-                    let idx = syn::Index::from(idx);
-                    (
-                        quote! {
-                            #idx: #field_name,
-                        },
-                        quote! {
-                            let _ =
-                                ::core::fmt::DebugTuple::field(
-                                    dbg_builder,
-                                    #field_name
-                                );
-                        },
-                    )
-                })
-            }
-        })
-        .unzip();
-
-    let start_fmtr = quote! {
-        let dbg_builder = &mut ::core::fmt::Formatter::debug_struct(fmtr, stringify!(#name));
-    };
-    let end_fmtr = quote! {
-        ::core::fmt::DebugStruct::finish(dbg_builder)
-    };
-    quote! {
-        let #name { #fields .. } = &self;
-        #start_fmtr
-        #debug_names
-        #end_fmtr
     }
 }
 
@@ -226,6 +265,37 @@ fn peel_path(ty: &Type) -> Vec<&syn::Path> {
     }
 }
 
-fn has_ignore(attrs: &[Attribute]) -> bool {
-    attrs.iter().any(|a| a.path.is_ident("dbg_ignore"))
+enum Attrs {
+    Ignore,
+    With(NestedMeta),
+    None,
+}
+
+fn has_attrs(attrs: &[Attribute]) -> syn::Result<Attrs> {
+    let mut ours = Attrs::None;
+
+    for attr in attrs {
+        if attr.path.is_ident("dbg_with") {
+            match attr.parse_meta()? {
+                Meta::List(meta) => match ours {
+                    Attrs::None => {
+                        ours = Attrs::With(meta.nested[0].clone());
+                    }
+                    _ => {
+                        return Err(syn::Error::new_spanned(
+                            meta,
+                            "invalid combination of attributes",
+                        ));
+                    }
+                },
+                bad => {
+                    return Err(syn::Error::new_spanned(bad, "unrecognized attribute"));
+                }
+            }
+        // TODO: catch error if this is already set
+        } else if attr.path.is_ident("dbg_ignore") {
+            ours = Attrs::Ignore;
+        }
+    }
+    Ok(ours)
 }
