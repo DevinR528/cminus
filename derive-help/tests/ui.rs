06cb16a59e2149473620c9a4907b90cec8e307d7
Devin Ragotzy <devin.ragotzy@gmail.com>
2021-11-21 07:42:37 -0500

    derive-help: add format with attribute
    Allows formatting a field with a given function/method

--- derive-help/tests/ui.rs
+++ derive-help/tests/ui.rs
@@ -3,6 +3,8 @@ fn compile_test() {
     let t = trybuild::TestCases::new();
     t.pass("tests/ui/enum.rs");
     t.pass("tests/ui/struct.rs");
+
+    t.compile_fail("tests/ui/with_call.rs");
 }
 
 fn main() {}
